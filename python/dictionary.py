284676f38f383f9ba2fd69507726d075ec0e5386
Sergeyk <sergey29204@gmail.com>
2024-10-15 12:51:56 +0300

    Counter imported and used

--- python/dictionary.py
+++ python/dictionary.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 # Создай список случайных чисел от 1 до 10, включающих повторяющиеся значения
 numlist2 = [random.randint(1, 10) for _ in range(20)]
 print("это список с повторяющимися значениями")
@@ -9,3 +10,8 @@ print(numdict)
 print("или в столбик")
 for key, value in numdict.items():
     print(f"{key}: {value}")
+
+print("или с использованием Counter:")
+numdict2 = Counter(numlist2)
+print(numdict2)
+
