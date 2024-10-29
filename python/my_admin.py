4ef21e23685a79bc4bc0d78092b08db84a91a72e
Sergey K. <sergey29204@gmail.com>
2024-10-29 19:47:46 +0200

    добавил новый файл

---
+++ python/my_admin.py
@@ -0,0 +1,11 @@
+import sys
+def main_loop():
+    
+    while(True):    
+        print("admin>", end='')
+        my_input = input()
+        print(my_input)
+        
+
+#sys.stdout.flush()
+main_loop()
