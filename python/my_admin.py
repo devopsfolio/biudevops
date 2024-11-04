1dcfd193994add0ef8035a6018d354420b56fba3
Sergey K. <sergey29204@gmail.com>
2024-11-04 17:34:32 +0200

    my_admin added

--- python/my_admin.py
+++ python/my_admin.py
@@ -1,7 +1,7 @@
 def main_loop():    
     while(True):    
-        print("admin> ", )
+        print("admin> ", end = '')
         my_input = input()
-        print(my_input)
+        print(my_input, end = '')
 
 main_loop()
